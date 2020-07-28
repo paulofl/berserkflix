8a6e5780ccb1c3650e96dfaa44ccb1538b0a5579
paulofl <paulofernandocornelio@gmail.com>
2020-07-28 20:41:35 -0300

    Setup Inicial do Projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://berserk.fandom.com/pt-br/wiki/P%C3%A1gina_principal">
+        <img src="https://2.bp.blogspot.com/-IcJNaNdNkPc/T07DrAz90OI/AAAAAAAAACQ/0G5RglK1cQY/s1600/Berserk%2819%29.jpg" alt="Marca do Sacrifício" />
+      </a>
+      <p>
+        Todos se deixam levar pela correnteza daquilo chamado destino ou coisa parecida, para no final desaparecerem do mundo, desperdiçando suas vidas, sem ao menos descobrirem o signigico de suas existências...
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
